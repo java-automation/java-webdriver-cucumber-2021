bcc6e88e97c58c6f9ae7f7a159d2de5652089107
Dmitry Igumnov <53914429+ehnat0n@users.noreply.github.com>
2021-11-10 22:33:02 -0700

    Added scenario/steps for minimal form submission (req), custom profiles

--- src/test/java/definitions/CustomStepDefs.java
+++ src/test/java/definitions/CustomStepDefs.java
@@ -2,11 +2,36 @@ package definitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
 
+import java.util.Locale;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
 import static support.TestContext.getDriver;
 
 public class CustomStepDefs {
+
+    private Map<String, String> profileOne = Map.ofEntries(
+            Map.entry("username", "jdoe"),
+            Map.entry("email", "john.doe@corp.com"),
+            Map.entry("password", "jsecret"),
+            Map.entry("name", "John Vitaljevich Doe")
+    );
+
+    private Map<String, String> profileTwo = Map.ofEntries(
+            Map.entry("username", "asmith"),
+            Map.entry("email", "alan.smith@corp.com"),
+            Map.entry("password", "asecret"),
+            Map.entry("name", "Alan Ivanovich Smith")
+    );
+
+    private Map<String, String> workingProfile;
+
     @Given("I go to {string} page")
     public void iGoToPage(String websiteReference) {
         String address = switch (websiteReference.toLowerCase()) {
@@ -61,5 +86,47 @@ public class CustomStepDefs {
         }
         getDriver().manage().window().setSize(new Dimension(width, height));
         Thread.sleep(1000);
+    }
+
+    @And("Submit the form")
+    public void submitTheForm() {
+        getDriver().findElement(By.xpath("//*[@id='formSubmit']")).click();
+    }
+
+    private void verifySummaryElementIsDisplayedAndHasValue(String summaryXPath, String value) {
+        WebElement usernameElement = getDriver().findElement(By.xpath(summaryXPath));
+        assertThat(usernameElement.isDisplayed()).isTrue();
+        assertThat(usernameElement.getText()).isEqualTo(value);
+    }
+
+    //This method assumes that workingProfile has been associated with one of the existing presets
+    @Then("I verify that submitted required fields got saved correctly")
+    public void iVerifyThatSubmittedFieldsGotSavedCorrectly() throws InterruptedException {
+        verifySummaryElementIsDisplayedAndHasValue("//*[@id='quotePageResult']//*[@name='username']", workingProfile.get("username"));
+        verifySummaryElementIsDisplayedAndHasValue("//*[@id='quotePageResult']//*[@name='email']", workingProfile.get("email"));
+        verifySummaryElementIsDisplayedAndHasValue("//*[@id='quotePageResult']//*[@name='password']", "[entered]");
+        verifySummaryElementIsDisplayedAndHasValue("//*[@id='quotePageResult']//*[@name='name']", workingProfile.get("name"));
+        verifySummaryElementIsDisplayedAndHasValue("//*[@id='quotePageResult']//*[@name='agreedToPrivacyPolicy']", "true");
+        Thread.sleep(3000);
+    }
+
+    private void setWorkingProfile(String profileReference) {
+        switch (profileReference.toLowerCase()) {
+            case "john doe" -> workingProfile = profileOne;
+            case "alan smith" -> workingProfile = profileTwo;
+            default -> throw new Error("Unknown profile reference: " + profileReference);
+        }
+    }
+
+    @When("I fill out required fields with {string} profile")
+    public void iFillOutRequiredFieldsWithProfile(String profileReference) throws InterruptedException {
+        setWorkingProfile(profileReference);
+        getDriver().findElement(By.xpath("//input[@name='username']")).sendKeys(workingProfile.get("username"));
+        getDriver().findElement(By.xpath("//input[@name='email']")).sendKeys(workingProfile.get("email"));
+        getDriver().findElement(By.xpath("//input[@name='password']")).sendKeys(workingProfile.get("password"));
+        getDriver().findElement(By.xpath("//input[@name='confirmPassword']")).sendKeys(workingProfile.get("password"));
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(workingProfile.get("name"));
+        getDriver().findElement(By.xpath("//input[@name='agreedToPrivacyPolicy']")).click();
+        Thread.sleep(3000);
     }
 }
--- src/test/resources/features/customQuote.feature
+++ src/test/resources/features/customQuote.feature
@@ -1,7 +1,7 @@
-@customWebDriverExercise
+@customQuote
 Feature: Selenium WebDriver API exercises using quote form
 
-  @customWebDriverExercise1
+  @customQuote1
   Scenario: Page details, navigation, resolution
     Given I go to "google" page
     And I print page details
@@ -10,3 +10,10 @@ Feature: Selenium WebDriver API exercises using quote form
     And I go back and forward, then refresh the page
     And I change resolution to "phone"
     And I change resolution to "desktop"
+
+  @customQuote2
+  Scenario: Submit the form with required fields only
+    Given I go to "quote form" page
+    When I fill out required fields with "Alan Smith" profile
+    And Submit the form
+    Then I verify that submitted required fields got saved correctly
