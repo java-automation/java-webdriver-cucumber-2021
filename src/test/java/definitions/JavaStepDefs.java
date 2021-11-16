3647ffee52860ece6d4dd06c093bd2ec1919ca93
VadimDzyma <vaadoos88@gmail.com>
2021-11-15 22:47:07 -0600

    My homework#6

---
+++ src/test/java/definitions/JavaStepDefs.java
@@ -0,0 +1,77 @@
+package definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class JavaStepDefs {
+    @Given("I write a function that prints all numbers from {int} up to {int}")
+    public void iWriteAFunctionThatPrintsAllNumbersFromUpTo(int num1, int num2) {
+        for (int i = num1; i <= num2; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+
+    @When("I write a function that supports also negative numbers from {int} to {int}")
+    public void iWriteAFunctionThatSupportsAlsoNegativeNumbersFromTo(int first, int last) {
+        for (int j = first; j <= last; j++) {
+            System.out.print(j + " ");
+        }
+    }
+
+    @Then("I write a function that prints all integer array")
+    public void iWriteAFunctionThatPrintsAllIntegerArray() {
+        int[] nums = {12, 5, 7, 1, 0, 5};
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    @And("I write a function that prints all even numbers from integer array")
+    public void iWriteAFunctionThatPrintsAllEvenNumbersFromIntegerArray() {
+        int[] arr = {6, 5, 8, 12, 1, 2, 18};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+
+    @And("I write a function that checks if array is empty")
+    public void iWriteAFunctionThatChecksIfArrayIsEmpty() {
+        int[] emptyarr = {3, 5, 1, 6, 10};
+        if (emptyarr.length == 0) {
+            System.out.print("Array is emply");
+        } else {
+            System.out.print("Array is not empty: ");
+            for (int i = 0; i < emptyarr.length; i++) {
+                System.out.print(emptyarr[i] + " ");
+            }
+        }
+    }
+
+    @And("Write a function that checks if array contains another element")
+    public void writeAFunctionThatChecksIfArrayContainsAnotherElement() {
+        List<Integer> elemcheck = new ArrayList<>();
+        elemcheck.add(5);
+        elemcheck.add(22);
+        elemcheck.add(4);
+        elemcheck.add(185);
+        elemcheck.add(3);
+        elemcheck.add(-9);
+        elemcheck.add(1);
+        elemcheck.add(1);
+        elemcheck.add(3);
+        System.out.print(elemcheck.contains(-9));
+    }
+}
+
+
---
+++ src/test/resources/features/java.feature
@@ -0,0 +1,15 @@
+@java
+  Feature: Java feature
+    @java1
+    Scenario: Hello World
+      Given I write a function that prints all numbers from 0 up to 12
+      When I write a function that supports also negative numbers from -5 to 5
+      Then I write a function that prints all integer array
+      And I write a function that prints all even numbers from integer array
+      And I write a function that checks if array is empty
+      And Write a function that checks if array contains another element
+
+
+
+
+
