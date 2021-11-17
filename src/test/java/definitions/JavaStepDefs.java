a432bf65b3afb0513ee533f8beba8f3c3e11a675
alex_ya <alegz.nn@gmail.com>
2021-11-16 23:16:45 -0800

    solutions for 5 first exercises for class 6

--- src/test/java/definitions/JavaStepDefs.java
+++ src/test/java/definitions/JavaStepDefs.java
@@ -3,19 +3,19 @@ package definitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 
-import java.sql.SQLOutput;
+import java.util.Arrays;
 
 public class JavaStepDefs {
     @Given("I hello world")
     public void iHelloWorld() {
-    System.out.println("Hello World!");
+        System.out.println("Hello World!");
 
-    String firstName = "John";
-    String lastName = "Doe";
-    String favoriteColor = "Green";
-    String text1 = "Hi, my name is";
-    String text2 = "my favorite color is";
-            System.out.println(text1 + " " + firstName + " " + lastName + ", " + text2 + " " + favoriteColor + ".");
+        String firstName = "John";
+        String lastName = "Doe";
+        String favoriteColor = "Green";
+        String text1 = "Hi, my name is";
+        String text2 = "my favorite color is";
+        System.out.println(text1 + " " + firstName + " " + lastName + ", " + text2 + " " + favoriteColor + ".");
     }
 
     @And("I say {string}")
@@ -38,13 +38,13 @@ public class JavaStepDefs {
 
     @And("I work with numbers {int} and {int}")
     public void iWorkWithNumbersAnd(int num1, int num2) {
-        System.out.println("Num 1: "+ num1);
-        System.out.println("Num 2: "+ num2);
+        System.out.println("Num 1: " + num1);
+        System.out.println("Num 2: " + num2);
         System.out.println(num1 % num2);
 
         if (num1 > num2) {
             System.out.println("Num1 is greater than Num2!");
-        } else if (num1==num2) {
+        } else if (num1 == num2) {
             System.out.println("Num1 is equal to Num2!");
         } else {
             System.out.println("Num1 is less than Num2!");
@@ -63,7 +63,7 @@ public class JavaStepDefs {
 
     @And("I work with loops")
     public void iWorkWithLoops() {
-        for (int i = 1; i <= 10; i = i + 1){
+        for (int i = 1; i <= 10; i = i + 1) {
             System.out.println(i);
         }
     }
@@ -78,4 +78,57 @@ public class JavaStepDefs {
             System.out.println("The number is zero");
         }
     }
+
+    @And("I print all numbers from {int} to {int}")
+    public void iPrintAllNumbersFromTo(int num1, int num2) {
+        for (int i = num1; i <= num2; i++) {
+            System.out.print(i+" ");
+        }
+    }
+
+    @And("I print all numbers including negative from {int} to {int}")
+    public void iPrintAllNumbersIncludingNegativeFromTo(int num1, int num2) {
+        for (int i = num1; i >= num2; i--) {
+            System.out.print(i+" ");
+        }
+    }
+
+      @And("I print integer array")
+    public void iPrintIntegerArray() {
+    int[] integer = {1, 4, 8, 9, 12, 3, 7, 5, 11};
+        System.out.println(Arrays.toString(integer));
+    }
+
+    @And("I print even numbers")
+    public void iPrintEvenNumbers() {
+        int[] nums = {3, 6, 8, 7, 5, 2, 11, 45, 20, 100};
+        for (int a = 0; a < nums.length; a++){
+            if (nums[a] % 2 == 0) {
+            System.out.print(nums[a] + " ");}
+
+        }
+    }
+
+    @And("I check for empty array")
+    public void iCheckForEmptyArray() {
+        int[] array1 = {};
+        int[] array2 = {1};
+        if (array1.length > 0)
+        {
+            System.out.println("array1 is not empty");
+        }
+        if (array1.length == 0)
+        {
+            System.out.println("array1 is empty");
+        }
+        if (array2.length > 0)
+        {
+            System.out.println("array2 is not empty");
+        }
+        else
+        {
+            System.out.println("array2 is empty");
+        }
+    }
 }
+
--- src/test/resources/features/java.feature
+++ src/test/resources/features/java.feature
@@ -14,3 +14,25 @@
 #  And I print url for "yahoo.com" page
       And I work with loops
       And I print if number -2 is positive
+
+      @java2
+      Scenario: Print numbers from 0 to n
+        And I print all numbers from 0 to 15
+
+   @java3
+   Scenario: Support negative numbers
+     And I print all numbers including negative from 5 to -5
+
+
+   @java4
+   Scenario: print integer array
+     And I print integer array
+
+   @java5
+   Scenario: print all even number from array
+     And I print even numbers
+
+   @java6
+   Scenario: check if array is empty
+     And I check for empty array
+
