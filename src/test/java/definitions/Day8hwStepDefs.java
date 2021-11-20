b9f94d02f15fb8862c0353df01d2a56773589bbe
VictorZ <vctvzv@gmail.com>
2021-11-19 16:40:10 -0800

    Day8_HW //Array. //Sort odd numbers in ascending order. //Even numbers must be on their places.

---
+++ src/test/java/definitions/Day8hwStepDefs.java
@@ -0,0 +1,34 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+import java.util.Arrays;
+
+public class Day8hwStepDefs {
+
+    @Given("I create an array, and sort odd numbers")
+    public void iCreateAnArrayAndSortOddNumbers() {
+        //int[] arr = {5, 3, 2, 8, 4, 1};
+        //int[] arr = {10, 5, 0, 7, 3, 2, 8, 4, 1};
+        int[] arr = {12, 10, 5, 0, 7, 3, 2, -1, 8, 4, 1};
+        System.out.println(Arrays.toString(arr));
+
+        for (int j = 0; j < arr.length - 1; j++) {
+            int idxMin = j;
+            int min = arr[idxMin];
+
+            for (int i = j + 1; i < arr.length; i++) {
+                if (arr[i] % 2 != 0 && (arr[i] < min)) {
+                    idxMin = i;
+                    min = arr[i];
+                }
+            }
+            if (arr[j] % 2 != 0 && arr[idxMin] % 2 != 0) {
+                int temp = arr[j];
+                arr[j] = min;
+                arr[idxMin] = temp;
+                System.out.println(Arrays.toString(arr));
+            }
+        }
+    }
+}
---
+++ src/test/resources/features/d8hwJava.feature
@@ -0,0 +1,6 @@
+@d8hwJava
+Feature: Java feature
+
+  @d8hwJava1
+  Scenario: sort odd numbers of array in ascending order
+    Given I create an array, and sort odd numbers
