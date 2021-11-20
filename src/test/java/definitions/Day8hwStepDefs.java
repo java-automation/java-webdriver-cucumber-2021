b66a50e64a353bc72da97fde551968f15a212de1
VictorZ <vctvzv@gmail.com>
2021-11-20 01:17:49 -0800

    Day8_HW //Solution #2

--- src/test/java/definitions/Day8hwStepDefs.java
+++ src/test/java/definitions/Day8hwStepDefs.java
@@ -31,4 +31,29 @@ public class Day8hwStepDefs {
             }
         }
     }
+
+    @Given("I create an array, and sort odd numbers in anthr way")
+    public void iCreateAnArrayAndSortOddNumbersInAnthrWay() {
+        //int[] arr = {5, 3, 2, 8, 4, 1};
+        //int[] arr = {10, 5, 0, 7, 3, 2, 8, 4, 1};
+        int[] arr = {12, 10, 5, 0, 7, 3, 2, -1, 8, 4, 1};
+        System.out.println("Before: " + Arrays.toString(arr));
+
+        for (int j = 0; j < arr.length - 1; j++) {
+            if (arr[j] % 2 != 0) {
+                int min = arr[j];
+
+                for (int i = j + 1; i < arr.length; i++) {
+                    if (arr[i] % 2 != 0 && (arr[i] < min)) {
+                        min = arr[i];
+
+                        int temp = arr[j];
+                        arr[j] = min;
+                        arr[i] = temp;
+                    }
+                }
+            }
+        }
+        System.out.println("After: " + Arrays.toString(arr));
+    }
 }
--- src/test/resources/features/d8hwJava.feature
+++ src/test/resources/features/d8hwJava.feature
@@ -4,3 +4,7 @@ Feature: Java feature
   @d8hwJava1
   Scenario: sort odd numbers of array in ascending order
     Given I create an array, and sort odd numbers
+
+  @d8hwJava2
+  Scenario: sort odd numbers of array in ascending order //v2
+    Given I create an array, and sort odd numbers in anthr way
