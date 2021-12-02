40c09d1feffaa64d4df07c611820eed4d76dbd5a
VictorZ <vctvzv@gmail.com>
2021-12-02 13:32:00 -0800

    Day10_HW
    //#1 swap two array elements
    //#2 check if n1 is divisible by n2 and n3
    //#3 find max element in an array
    //#4 FizzBuzz
    //#5 reverse string
    //#6 reverse words in a sentence

---
+++ src/test/java/definitions/Day10hwStepDefs.java
@@ -0,0 +1,102 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+import java.util.Arrays;
+
+public class Day10hwStepDefs {
+
+    //#1
+    @Given("I swap two array elements {int} and {int}")
+    public void iSwapTwoArrayElements(int j, int k) {
+        int[] ar1 = {5, 2, 9, 7, 3};
+        System.out.println("Before: " + Arrays.toString(ar1));
+
+        ar1[j - 1] = ar1[j - 1] * ar1[k - 1];
+        ar1[k - 1] = ar1[j - 1] / ar1[k - 1];
+        ar1[j - 1] = ar1[j - 1] / ar1[k - 1];
+
+        System.out.println("After: " + Arrays.toString(ar1));
+    }
+
+    //#2
+    @Given("I check if {int} is divisible by {int} and {int}")
+    public void checkIfNumberDivisibleBy(int dividend, int firstDiv, int secDiv) {
+        if (dividend % firstDiv == 0 && dividend % secDiv == 0) {
+            System.out.println("Dividend: " + dividend + "; divisible by: " + firstDiv + " and " + secDiv);
+            return;
+        }
+        if (dividend % firstDiv == 0) {
+            System.out.println("Dividend: " + dividend + "; divisible by: " + firstDiv);
+            return;
+        }
+        if (dividend % secDiv == 0) {
+            System.out.println("Dividend: " + dividend + "; divisible by: " + secDiv);
+        } else System.out.println("Dividend: " + dividend + "; not divisible by: " + firstDiv + " and " + secDiv);
+    }
+
+    //#3
+    @Given("I get max element in an array")
+    public void getMaxElement() {
+        int[] ar1 = {5, 2, 9, 7, 3};
+        System.out.println("Array: " + Arrays.toString(ar1));
+
+        int maxElem = ar1[0];
+        for (int i = 1; i < ar1.length; i++) {
+            if (ar1[i] > maxElem) {
+                maxElem = ar1[i];
+            }
+        }
+        System.out.println("MaxElement: " + maxElem);
+    }
+
+    //#4
+    @Given("I print all the elements from {int} to {int} but multiples of {int} and {int}")
+    public void printElementsAndFizzBuzz(int start, int end, int multiple1, int multiple2) {
+        for (int i = start; i <= end; i++) {
+            if (i % multiple1 == 0 && i % multiple2 == 0) {
+                System.out.print("FizzBuzz ");
+                continue;
+            }
+            if (i % multiple1 == 0) {
+                System.out.print("Fizz ");
+                continue;
+            }
+            if (i % multiple2 == 0) {
+                System.out.print("Buzz ");
+            } else System.out.print(i + " ");
+        }
+    }
+
+    //#5
+    @Given("I reverse string {string}")
+    public void reverseString(String name) {
+        System.out.println("String before: " + name);
+
+        String revName = "";
+        for (int i = name.length() - 1; i >= 0; i--) {
+            revName += name.charAt(i);
+        }
+        System.out.println("String after: " + revName);
+    }
+
+    //#6
+    @Given("I reverse words in a sentence {string}")
+    public void reverseSentenceWords(String sentence) {
+        System.out.println("Sentence before: " + sentence);
+
+        String[] strToArr = sentence.split(" ");
+        String[] strToArr2 = new String[strToArr.length];
+
+        for (int i = 0; i < strToArr.length; i++) {
+            StringBuilder reverseName = new StringBuilder(strToArr[i]);
+            reverseName.reverse();
+            strToArr2[i] = reverseName.toString();
+        }
+        System.out.println("Arrays.toString: " + Arrays.toString(strToArr2));
+        System.out.println("Sentence after: " + String.join(" ", strToArr2));
+    }
+}
+
+
+
---
+++ src/test/resources/features/d10_hw_java.feature
@@ -0,0 +1,28 @@
+@d10_hw_Java
+Feature: Java feature
+
+  @d10_hw_Java1
+  Scenario: Function that swaps two array elements // 3rd and 5th
+   Given I swap two array elements 3 and 5
+
+  @d10_hw_Java2
+  Scenario: Function that prints "divisible by num" if number is divisible
+    Given I check if 432 is divisible by 3 and 4
+
+  @d10_hw_Java3
+  Scenario: Function to find the largest element in an array
+    Given I get max element in an array
+
+  @d10_hw_Java4
+  Scenario: Function prints all the numbers from start to end.
+  If multiple of n1 or n2  prints Fizz, Buzz and FizzBuzz
+    Given I print all the elements from 1 to 20 but multiples of 3 and 5
+
+  @d10_hw_Java5
+  Scenario: Function that reverses string
+    Given I reverse string "KAYAK"
+
+  @d10_hw_Java6
+  Scenario: Function that reverses words in a sentence
+    Given I reverse words in a sentence "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+
