9b06bb650e8b6314999e54b013250dc8003b4e48
Marina A. Shaposhnik <marina.shaposhnik@gmail.com>
2021-12-02 18:36:01 -0800

    Add steps and step def for coding challenges
    Homework #9 (Lesson 9, 11/29/2021)
    FizzBuzz, string and sentence reversal, array manipulations

--- src/test/java/definitions/JavaStepDefs.java
+++ src/test/java/definitions/JavaStepDefs.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Keys;
@@ -13,6 +14,7 @@ import org.openqa.selenium.Keys;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -281,5 +283,193 @@ public class JavaStepDefs {
     public void arrayAndArrayAreJoinedTogetherAlternatingInto(int[] arr1, String[] s_arr2, String[] result) {
         List<String> list_arr1 = Arrays.stream(arr1).mapToObj(i -> String.valueOf(i)).toList();
         assertThat(joinListsAlternating(list_arr1, Arrays.asList(s_arr2))).isEqualTo(Arrays.asList(result));
+    }
+
+    @Then("do FizzBuzz printing by method 1 for number {int}")
+    public void fizzBuzz1(int num) {
+        long start = System.nanoTime();
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+        if (num >= 1) {
+            System.out.println("\b");
+        }
+        long stop = System.nanoTime();
+        System.out.println("Elapsed time: " + (stop - start));
+    }
+
+    @Then("do FizzBuzz printing by method 2 for number {int}")
+    public void fizzBuzz2(int num) {
+        long start = System.nanoTime();
+        for (int i = 1; i <= num; i++) {
+            if (i % 5 != 0) {
+                if (i % 3 != 0) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print("Fizz ");
+                }
+            } else if (i % 3 != 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print("FizzBuzz ");
+            }
+        }
+        if (num >= 1) {
+            System.out.println("\b");
+        }
+        long stop = System.nanoTime();
+        System.out.println("Elapsed time: " + (stop - start));
+    }
+
+    @Then("do FizzBuzz printing by method 3 for number {int}")
+    public void fizzBuzz3(int num) {
+        long start = System.nanoTime();
+        String s = "";
+        for (int i = 1; i <= num; i++) {
+            s = "";
+            if (i % 3 == 0) s += "Fizz";
+            if (i % 5 == 0) s += "Buzz";
+            if (s.isEmpty()) s += i;
+            System.out.print(s + " ");
+        }
+        System.out.println("\b");
+        long stop = System.nanoTime();
+        System.out.println("Elapsed time: " + (stop - start));
+    }
+
+    @Then("do FizzBuzz printing by method 4 for number {int}")
+    public void fizzBuzz4(int num) {
+        long start = System.nanoTime();
+        int n = 0;
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+                n = 1;
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+                n = 1;
+            }
+            if (n == 0) System.out.print(i);
+            System.out.print(" ");
+            n = 0;
+        }
+        System.out.println("\b");
+        long stop = System.nanoTime();
+        System.out.println("Elapsed time: " + (stop - start));
+    }
+
+    private void swapInArray(int[] arr, int idx1, int idx2) {
+        if (idx1 == idx2) return;
+        if (idx1 < 0 || idx1 >= arr.length || idx2 < 0 || idx2 >= arr.length) {
+            throw new Error("Index(es) is outside of the arrays boundaries");
+        }
+        arr[idx1] = arr[idx1] + arr[idx2];
+        arr[idx2] = arr[idx1] - arr[idx2];
+        arr[idx1] = arr[idx1] - arr[idx2];
+    }
+
+    @Then("reverse string {string}")
+    public String reverseUnicodeString(String str) {
+        System.out.println("Input string: " + str + " with length in Unicode code units: " + str.length());
+        System.out.println("Input string escaped for printing only: " + StringEscapeUtils.escapeJava(str));
+        int[] int_array = str.codePoints().toArray();
+        int len = int_array.length;
+        // Java internal String representations uses UTF16 encoding [16 is the size of a code unit in bits].
+        // A code point is the value that a Unicode (UCS) character is mapped to in the Unicode standard.
+        // In UTF16 (Unicode Transformation Format) code points are encoded with 1 or 2 code units.
+        // For example, the flat note symbol ♭ has a code point of U+1D160 and would be encoded using
+        // the combination of two 16-bit code units U+D834 and U+DD60 in UTF16 (high-low surrogate pair).
+        System.out.println("Input string length in Unicode code points: " + len);
+        for (int i = 0; i < len/2; i++) {
+            swapInArray(int_array,i,len-1-i);
+        }
+        String reversed_str = new String(int_array, 0, len);
+        System.out.println("Reversed string: " + reversed_str +
+                           " with length in Unicode code units: " + reversed_str.length());
+        // StringEscapeUtils.escapeJava method deals correctly with quotes and control-chars,
+        // so a tab becomes the characters '\\' and 't'.
+        System.out.println("Reversed escaped string for printing only: " + StringEscapeUtils.escapeJava(reversed_str));
+        return reversed_str;
+    }
+
+    @Then("reverse string {string} via StringBuffer")
+    public String reverseString2(String str) {
+        String reversed_str = new StringBuffer(str).reverse().toString();
+        System.out.println("Reversed string: " + reversed_str);
+        return reversed_str;
+    }
+
+    @Then("reverse string {string} unescaped")
+    public String reverseStringUnescaped(String str) {
+        // For example, StringEscapeUtils.unescapeJava will turn a sequence of '\' and 'n' into a newline character,
+        // unless the '\' is preceded by another '\'
+        String unescaped_str = StringEscapeUtils.unescapeJava(str);
+        String reversed_str = reverseUnicodeString(unescaped_str);
+        System.out.println("Reversed string escaped for printing only: " + StringEscapeUtils.escapeJava(reversed_str));
+        return reversed_str;
+    }
+
+    @Then("reverse common string {string}")
+    public String reverseCommonString(String str) {
+        System.out.println("Input string: " + str + " with length in Unicode code units: " + str.length());
+        System.out.println("Input string escaped for printing only: " + StringEscapeUtils.escapeJava(str));
+        char[] char_array = str.toCharArray();
+        int len = char_array.length;
+        System.out.println("Input string: " + str + " with length in chars (units of 2 bytes): " + str.length());
+        char temp;
+        for (int i = 0; i < len / 2; i++) {
+            temp = char_array[i];
+            char_array[i] = char_array[len-1-i];
+            char_array[len-1-i] = temp;
+        }
+        String reversed_str = new String(char_array);
+        System.out.println("Reversed string: " + reversed_str +
+                           " with length in Unicode code units: " + reversed_str.length() );
+        System.out.println("Reversed escaped string for printing only: " + StringEscapeUtils.escapeJava(reversed_str));
+        return reversed_str;
+    }
+
+    @Then("reverse words in sentence {string}")
+    public void reverseWordsInSentence(String str) {
+        String[] str_array = StringEscapeUtils.unescapeJava(str).split("\\W+");
+        int len = str_array.length;
+        for (int i=0; i<len/2; i++) {
+            str_array[len-1-i] = str_array[i] + ( str_array[i] = str_array[len-1-i] ).substring(0,0);
+        }
+        // alternatively via stream
+        // String[] revered_words = (String[]) IntStream.range(0,len).mapToObj(i -> str_array[len-1-i]).toArray());
+        System.out.println(String.join(" ", str_array));
+    }
+
+    @Then("swaps two elements of array {int_array} in positions {int} and {int}")
+    public void swapsTwoElementsOfArrayRdAndTh(int[] arr, int pos1, int pos2) {
+        if (pos1 < 1 || pos1 > arr.length || pos2 < 1 || pos2 > arr.length ) {
+            System.out.println("Swap is not applicable for passed positions " + pos1 + " and " + pos2);
+        } else {
+            swapInArray(arr, pos1 - 1, pos2 - 1);
+            System.out.println("Resulting array: " + Arrays.toString(arr));
+        }
+    }
+
+    @Then("largest element in an array {int_array}")
+    public void largestElementInAnArray(int[] arr) {
+        if (arr==null || arr.length==0) {
+            System.out.println("No max value in an empty array");
+            return;
+        }
+        int max=Integer.MIN_VALUE;
+        for (int el : arr) {
+            if (max < el) max = el;
+        }
+        System.out.println("Max value: " + max);
     }
 }
--- src/test/resources/features/java.feature
+++ src/test/resources/features/java.feature
@@ -79,10 +79,97 @@ Feature: Java feature
     Then array {0,1,67,-1,3,8,10,38,5,1,0,13} sorted by method "sortIfOdd4" should match {0, -1, 1, 1, 3, 8, 10, 38, 5, 13, 0, 67}
 
   @java13:
-  Scenario: Joining arrays alternating
-    Then array {1,5,8,3,6} and array {8,3,6} are joined together alternating into {1,8,5,3,8,6,3,6}
-    Then list {5,34,0} and list {0,3,0,-12,-4} are joined together alternating into {5,0,34,3,0,0,-12,-4}
-    Then array {1,2,3,4} and array {"a","b","c"} are joined together alternating into {"1","a","2","b","3","c","4"}
+  Scenario: Joining arrays alternating coding challenge
+#    Then array {1,5,8,3,6} and array {8,3,6} are joined together alternating into {1,8,5,3,8,6,3,6}
+#    Then list {5,34,0} and list {0,3,0,-12,-4} are joined together alternating into {5,0,34,3,0,0,-12,-4}
+#    Then array {1,2,3,4} and array {"a","b","c"} are joined together alternating into {"1","a","2","b","3","c","4"}
+    Then array {} and array {8,3,6} are joined together alternating into {8,3,6}
+    Then array {1,2,3,4} and array {""} are joined together alternating into {"1","","2","3","4"}
+#    Then array {1,2,3,4} and array {} are joined together alternating into {"1","","2","3","4"}
+
+  @java14:
+  Scenario: FizzBuzz coding challenge
+      Then do FizzBuzz printing by method 1 for number 10000
+      Then do FizzBuzz printing by method 2 for number 10000
+      Then do FizzBuzz printing by method 3 for number 10000
+      Then do FizzBuzz printing by method 4 for number 10000
+      Then do FizzBuzz printing by method 1 for number 0
+      Then do FizzBuzz printing by method 1 for number -1
+      Then do FizzBuzz printing by method 1 for number 1
+      Then do FizzBuzz printing by method 1 for number 2
+
+  @java15:
+  Scenario: Reverse string coding challenge
+    Then reverse string "a"
+    Then reverse string "Z"
+    Then reverse string "0123456789"
+    Then reverse string "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    Then reverse string "a\b+c"
+    Then reverse string "a\\b+c"
+    Then reverse string "\t\b\n\r\f\'\"\\"
+    Then reverse string "😂😃🧘️🌍🍞🚗"
+    Then reverse string "🧘️"
+    Then reverse string "🍞"
+    Then reverse string "qwerty"
+    Then reverse string "qwe1rty"
+    Then reverse string "🍞"
+    # with invisible Unicode Character 'VARIATION SELECTOR-16' (U+FE0F) (cursor stays still when pressing arrow button)
+    Then reverse string "🧘️"
+    Then reverse string "🧘️" via StringBuffer
+    Then reverse string "a\b+c" unescaped
+    Then reverse string "\t\b\n\r\f\'\"\\" unescaped
+    Then reverse string "\uD83E\uDDD8" unescaped
+
+    Then reverse string "\""
+    Then reverse string "\"" via StringBuffer
+    Then reverse string "\"" unescaped
+    Then reverse common string "\""
+
+    Then reverse string "\'"
+    Then reverse string "\'" via StringBuffer
+    Then reverse string "\'" unescaped
+    Then reverse common string "\'"
+
+    Then reverse string "😂, 😃, 🧘️, 🌍, 🍞, 🚗"
+    Then reverse common string "😂, 😃, 🧘️, 🌍, 🍞, 🚗"
+
+    Then reverse string "%s"
+    Then reverse string "! #$%&*+-/=?^_`{}~',.~!@#$%^&*()_+=-`,.<>?/:\";'{}\|]["
+    Then reverse string "          "
+    Then reverse string ""
+    Then reverse string "überwintern"
+    Then reverse string "\u0048\u0047\u0046\u0045"
+    Then reverse string "\\u0048\\u0047\\u0046\\u0045"
+    Then reverse string "\u2640" unescaped
+    Then reverse string "\u0048\u0047\u0046\u0045" unescaped
+
+    # Woman Golfing: Light Skin Tone
+    # ZWJ sequence combining 🏌️ Person Golfing, 🏻 Light Skin Tone, ‍ Zero Width Joiner and ♀️ Female Sign.
+    Then reverse string "🏌🏻‍♀"
+
+  @java16:
+  Scenario: Reverse words in sentence coding challenge
+      Then reverse words in sentence "Lorem ipsum dolor sit amet, consectetur 123 adipiscing   elit."
+      Then reverse words in sentence "Have\t a nice day! 😃\n"
+      Then reverse words in sentence "\u0048\u0047\u0046\u0045"
+
+  @java17
+  Scenario: Swap elements of the array
+      Then swaps two elements of array {5,2,9,7,3} in positions 3 and 5
+      Then swaps two elements of array {5,2,9,7,3} in positions 0 and 5
+      Then swaps two elements of array {5,2,9,7,3} in positions 3 and 6
+      Then swaps two elements of array {5,2,9,7,3} in positions 2 and 2
+      Then swaps two elements of array {5,2,9,7,3} in positions -1 and 5
+
+  @java18
+  Scenario: Largest element in array
+    Then largest element in an array {3, -1, 0, 374}
+    Then largest element in an array {-2147483648}
+    Then largest element in an array {-2147483648, 2147483647}
+    Then largest element in an array {0, 0, 0}
+    Then largest element in an array {344, 1, 85}
+    Then largest element in an array {-284, -4}
+    Then largest element in an array {}
 
 
 
