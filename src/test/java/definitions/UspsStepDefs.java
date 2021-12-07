216d3ac1df84e2987715c244e39a97cde77ac63a
Dmitry Igumnov <53914429+ehnat0n@users.noreply.github.com>
2021-12-07 12:06:13 -0700

    Added 2 strategies for ZIP look up page

--- src/test/java/definitions/UspsStepDefs.java
+++ src/test/java/definitions/UspsStepDefs.java
@@ -1,11 +1,28 @@
 package definitions;
 
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+
+import static support.TestContext.getDriver;
 
 public class UspsStepDefs {
 
-    @When("I go to Lookup ZIP page by address")
-    public void iGoToLookupZIPPageByAddress() {
-
+    @When("I go to Lookup ZIP page by address through {string}")
+    public void iGoToLookupZIPPageByAddress(String strategy) throws InterruptedException {
+        if (strategy.equals("quick tools")) {
+            getDriver().findElement(By.xpath("//ul[@class='nav-list']//a[contains(@class,'nav-first-element')]")).click();
+            Thread.sleep(1000);
+            getDriver().findElement(By.xpath("//a[@role='menuitem']/img[contains(@alt,'Zip Code')]")).click();
+            Thread.sleep(1000);
+            getDriver().findElement(By.xpath("(//a[contains(@class,'btn-primary')][@data-location='byaddress'])[1]")).click();
+            Thread.sleep(1000);
+        } else if (strategy.equals("send")) {
+            getDriver().findElement(By.xpath("//a[@id='mail-ship-width']")).click();
+            Thread.sleep(1000);
+            getDriver().findElement(By.xpath("//h2/a[contains(normalize-space(text()),'Look Up a ZIP Code')]")).click();
+            Thread.sleep(1000);
+            getDriver().findElement(By.xpath("(//a[contains(@class,'btn-primary')][@data-location='byaddress'])[1]")).click();
+            Thread.sleep(1000);
+        } else throw new Error("Unknown strategy: " + strategy);
     }
 }
--- src/test/resources/features/usps.feature
+++ src/test/resources/features/usps.feature
@@ -4,6 +4,6 @@ Feature: USPS website scenarios
   @usps1
   Scenario: Validate ZIP code for Portnov Computer School
     Given I go to "usps" page
-    When  I go to Lookup ZIP page by address
+    When  I go to Lookup ZIP page by address through "send"
     *     I fill out "4970 El Camino Real" street, "Los Altos" city, "CA" state
     Then  I validate "94022" zip code exists in the result
