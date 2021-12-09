f038f13cd18b2eba8f6ddc86d5e1c16b35dc9cea
Olga Casian <casianolga@gmail.com>
2021-12-08 20:17:22 -0600

    Day 9 - usps scenario

---
+++ src/test/java/definitions/UspsStepDefs.java
@@ -0,0 +1,34 @@
+package definitions;
+
+import io.cucumber.java.en.*;
+import io.cucumber.java8.*;
+import org.assertj.core.api.*;
+import org.junit.*;
+import org.openqa.selenium.*;
+
+import static support.TestContext.getDriver;
+
+public class UspsStepDefs {
+    @When("I go to Lookup ZIP page by address")
+    public void iGoToLookupZIPPageByAddress() throws InterruptedException {
+        getDriver().findElement(By.xpath("//*[@id='navquicktools']/..")).click();
+        getDriver().findElement(By.xpath("//p[contains(text(),'Look Up')]")).click();
+        Assertions.assertThat(getDriver().getTitle().toLowerCase()).contains("zip code");
+    }
+
+    @And("I fill out {string} street, {string} city, {string} state")
+    public void iFillOutStreetCityState(String street, String city, String state) {
+        getDriver().findElement(By.xpath("//a[@data-location='byaddress'][contains(text(),'By Address')]")).click();
+        getDriver().findElement(By.xpath("//input[@id='tAddress']")).sendKeys(street);
+        getDriver().findElement(By.xpath("//input[@id='tCity']")).sendKeys(city);
+        getDriver().findElement(By.xpath("//select[@id='tState']//option[@value='" + state + "']")).click();
+    }
+
+    @Then("I validate {string} zip code exists in the result")
+    public void iValidateZipCodeExistsInTheResult(String zip) throws InterruptedException {
+        getDriver().findElement(By.xpath("//a[@id='zip-by-address']")).click();
+       // getDriver().findElement(By.xpath("//ul[contains(@class,'entered-address')]")).isDisplayed();
+        Thread.sleep(2000);
+        Assertions.assertThat(getDriver().findElement(By.xpath("//ul[contains(@class,'industry-detail')]//li[1]")).getText()).contains(zip);
+    }
+}
---
+++ src/test/resources/features/usps.feature
@@ -0,0 +1,9 @@
+@usps
+  Feature: Usps scenarios
+
+    @usps1
+    Scenario: Validate zip code for address
+      Given I go to "usps" page and print details
+      When I go to Lookup ZIP page by address
+      And I fill out "4970 El Camino Real" street, "Los Altos" city, "CA" state
+      Then I validate "94022" zip code exists in the result
