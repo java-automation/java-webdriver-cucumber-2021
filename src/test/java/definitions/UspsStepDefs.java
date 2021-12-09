3f17517d2b281e3946bfca37a413aa010d0d7d00
ksyusha1 <ksyusha1@gmail.com>
2021-12-09 14:11:21 -0500

    Quote and usps

--- src/test/java/definitions/UspsStepDefs.java
+++ src/test/java/definitions/UspsStepDefs.java
@@ -7,7 +7,10 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 import static support.TestContext.getDriver;
+import static org.assertj.core.api.AbstractBooleanAssert.setPrintAssertionsDescription;
+import org.openqa.selenium.WebElement;
 import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
 
 public class UspsStepDefs {
     Actions action = new Actions(getDriver());
@@ -35,6 +38,7 @@ public class UspsStepDefs {
 
     @And("I fill out {string} street, {string} city, {string} state")
     public void iFillOutStreetCityState(String street, String city, String state) throws InterruptedException {
+
         getDriver().findElement(By.xpath("//input[@id='tAddress']")).sendKeys(street);
         getDriver().findElement(By.xpath("//input[@id='tCity']")).sendKeys(city);
 
@@ -60,14 +64,31 @@ public class UspsStepDefs {
       //  System.out.println("Incorrect address Error: "+ errorMessage);
         // //div[contains(@class,'server-error address-tCity')]
 
-        String res=getDriver().findElement(By.xpath("//div[@id='zipByAddressDiv']//strong")).getText();
-        String resTest1=getDriver().findElement(By.xpath("//div[@id='zipByAddressDiv']")).getText(); // w/out //strong RETURNS ALL ADDRESSES
-        System.out.println("Zip Code for provided address: " + res);
+// Assertion 1
+//        String res=getDriver().findElement(By.xpath("//div[@id='zipByAddressDiv']//strong")).getText();
+//        String resTest1=getDriver().findElement(By.xpath("//div[@id='zipByAddressDiv']")).getText(); // w/out //strong RETURNS ALL ADDRESSES
+//        System.out.println("Zip Code for provided address: " + res);
+//        assertThat(resTest1).contains(zipCode);  // WHY TEST "PASSED" If assertion is incorrect ?!
 
+        // Assertion 3 (Should be modified ? )
+          List<WebElement> results = getDriver().findElements(By.xpath("//*[@class='zipcode-result-address']//strong"));
+
+
+          for (WebElement res: results){
+              System.out.println(res.getText());
+              if (!res.getText().contains(zipCode)){
+                  throw new Error ("Assertion failed: " + res.getText());
+              }
+          }
+
+
+// Assertion 2
 //        if (!resTest1.contains(zipCode)){
 //           throw new Error ("Result doesn't contain the expected zip code: "+zipCode);
 //        }
-        assertThat(resTest1).contains(zipCode);  // WHY TEST "PASSED" If assertion is incorrect ?!
+
+
+
 
     }
 }
