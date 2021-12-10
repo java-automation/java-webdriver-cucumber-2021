a0539c8e88a4d6cd334e20167742637bc217e96d
Gavrilova <gavrilova.irina@gmail.com>
2021-12-09 18:16:10 -0600

    In 'USPS' scenario added cases with difference as how we open 'Look up a ZIP CODE' page: - by moving cursor to the navigation panel Quick Tools->Look Up a ZIP CODE; - through hamburger menu; - when window were resized and USPS start page opens with 'QUICK TOOLS TRACKING OPTION' section.

--- src/test/java/definitions/PredefinedStepDefs.java
+++ src/test/java/definitions/PredefinedStepDefs.java
@@ -18,220 +18,223 @@ import static org.assertj.core.api.Assertions.*;
 import static support.TestContext.getDriver;
 
 public class PredefinedStepDefs {
-  @Given("I open url {string}")
-  public void iOpenUrl(String url) {
-    getDriver().get(url);
-  }
-
-  @Then("I resize window to {int} and {int}")
-  public static void iResizeWindowToAnd(int width, int height) {
-    Dimension dimension = new Dimension(width, height);
-    getDriver().manage().window().setSize(dimension);
-  }
-
-  @Then("I wait for {int} sec")
-  public void iWaitForSec(int sec) throws Exception {
-    Thread.sleep(sec * 1000);
-  }
-
-  @When("I maximize window")
-  public static void iMaximizeWindow() {
-    java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-    Dimension maxWindowSize = new Dimension((int) screenSize.getWidth(), (int) screenSize.getHeight());
-    getDriver().manage().window().setPosition(new Point(0, 0));
-    getDriver().manage().window().setSize(maxWindowSize);
-  }
-
-  @Then("element with xpath {string} should be present")
-  public void elementWithXpathShouldBePresent(String xpath) {
-    assertThat(getDriver().findElements(By.xpath(xpath))).hasSize(1);
-  }
-
-  @Then("element with xpath {string} should not be present")
-  public void elementWithXpathShouldNotBePresent(String xpath) {
-    assertThat(getDriver().findElements(By.xpath(xpath))).hasSize(0);
-  }
-
-  @Then("I wait for element with xpath {string} to be present")
-  public static void iWaitForElementWithXpathToBePresent(String xpath) {
-    new WebDriverWait(getDriver(), 10, 200).until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
-  }
-
-
-  @Then("I wait for element with xpath {string} to not be present")
-  public static void iWaitForElementWithXpathToNotBePresent(String xpath) {
-    new WebDriverWait(getDriver(), 10, 200).until(ExpectedConditions.not(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath))));
-  }
-
-  @Then("element with xpath {string} should be displayed")
-  public static void elementWithXpathShouldBeDisplayed(String xpath) {
-    assertThat(getDriver().findElement(By.xpath(xpath)).isDisplayed()).isTrue();
-  }
-
-  @Then("element with xpath {string} should not be displayed")
-  public void elementWithXpathShouldNotBeDisplayed(String xpath) {
-    assertThat(getDriver().findElement(By.xpath(xpath)).isDisplayed()).isFalse();
-  }
-
-  @Then("element with xpath {string} should be enabled")
-  public void elementWithXpathShouldBeEnabled(String xpath) {
-    assertThat(getDriver().findElement(By.xpath(xpath)).isEnabled()).isTrue();
-  }
-
-  @Then("element with xpath {string} should be disabled")
-  public void elementWithXpathShouldBeDisabled(String xpath) {
-    assertThat(getDriver().findElement(By.xpath(xpath)).isEnabled()).isFalse();
-  }
-
-  @Then("element with xpath {string} should be selected")
-  public void elementWithXpathShouldBeSelected(String xpath) {
-    assertThat(getDriver().findElement(By.xpath(xpath)).isSelected()).isTrue();
-  }
-
-  @Then("element with xpath {string} should not be selected")
-  public void elementWithXpathShouldNotBeSelected(String xpath) {
-    assertThat(getDriver().findElement(By.xpath(xpath)).isSelected()).isFalse();
-  }
-
-  @When("I type {string} into element with xpath {string}")
-  public void iTypeIntoElementWithXpath(String text, String xpath) {
-    getDriver().findElement(By.xpath(xpath)).sendKeys(text);
-  }
-
-  @Then("I click on element with xpath {string}")
-  public static void iClickOnElementWithXpath(String xpath) {
-    getDriver().findElement(By.xpath(xpath)).click();
-  }
-
-  @Then("I click on element using JavaScript with xpath {string}")
-  public void iClickOnElementUsingJavaScriptWithXpath(String xpath) {
-    WebElement element = getDriver().findElement(By.xpath(xpath));
-    JavascriptExecutor executor = (JavascriptExecutor) getDriver();
-    executor.executeScript("arguments[0].click();", element);
-  }
-
-  @Then("I take screenshot")
-  public void iTakeScreenshot() throws Exception {
-    TakesScreenshot screenshotTaker = (TakesScreenshot) getDriver();
-    File screenshot = screenshotTaker.getScreenshotAs(OutputType.FILE);
-    FileUtils.copyFile(screenshot, new File("target/screenshot " + new Date() + ".png"));
-  }
-
-  @Then("element with xpath {string} should have text as {string}")
-  public void elementWithXpathShouldHaveTextAs(String xpath, String text) {
-    String actualText = getDriver().findElement(By.xpath(xpath)).getText();
-    assertThat(actualText).isEqualTo(text);
-  }
-
-  @Then("element with xpath {string} should not have text as {string}")
-  public void elementWithXpathShouldNotHaveTextAs(String xpath, String text) {
-    String actualText = getDriver().findElement(By.xpath(xpath)).getText();
-    assertThat(actualText).isNotEqualTo(text);
-  }
-
-  @Then("element with xpath {string} should contain text {string}")
-  public void elementWithXpathShouldContainText(String xpath, String text) {
-    String actualText = getDriver().findElement(By.xpath(xpath)).getText();
-   // String textContent = getDriver().findElement(By.xpath(xpath)).getAttribute("textContent");
-   // String innerHTML = getDriver().findElement(By.xpath(xpath)).getAttribute("innerHTML");
-    assertThat(actualText).containsIgnoringCase(text);
-  }
-
-  @Then("element with xpath {string} should not contain text {string}")
-  public void elementWithXpathShouldNotContainText(String xpath, String text) {
-    String actualText = getDriver().findElement(By.xpath(xpath)).getText();
-    assertThat(actualText).doesNotContain(text);
-  }
-
-  @Then("element with xpath {string} should have attribute {string} as {string}")
-  public void elementWithXpathShouldHaveAttributeAs(String xpath, String attribute, String attributeValue) {
-    String value = getDriver().findElement(By.xpath(xpath)).getAttribute(attribute);
-    String attrValue = attributeValue;
-    assertThat(getDriver().findElement(By.xpath(xpath)).getAttribute(attribute)).isEqualTo(attributeValue);
-  }
-
-  @Then("element with xpath {string} should not have attribute {string} as {string}")
-  public void elementWithXpathShouldNotHaveAttributeAs(String xpath, String attribute, String attributeValue) {
-    assertThat(getDriver().findElement(By.xpath(xpath)).getAttribute(attribute)).isNotEqualTo(attributeValue);
-  }
-
-  @Then("I switch to iframe with xpath {string}")
-  public void iSwitchToIframeWithXpath(String xpath) {
-    getDriver().switchTo().frame(getDriver().findElement(By.xpath(xpath)));
-  }
-
-  @Then("I switch to default content")
-  public void iSwitchToDefaultContent() {
-    getDriver().switchTo().defaultContent();
-  }
-
-  @Then("I switch to new window")
-  public void iSwitchToNewWindow() {
-    Iterator<String> iterator = getDriver().getWindowHandles().iterator();
-    String newWindow = iterator.next();
-    while (iterator.hasNext()) {
-      newWindow = iterator.next();
+    @Given("I open url {string}")
+    public static void iOpenUrl(String url) {
+        getDriver().get(url);
     }
-    getDriver().switchTo().window(newWindow);
-  }
 
-  @Then("I switch to first window")
-  public void iSwitchToFirstWindow() {
-    getDriver().switchTo().window(getDriver().getWindowHandles().iterator().next());
-  }
+    @Then("I resize window to {int} and {int}")
+    public static void iResizeWindowToAnd(int width, int height) {
+        Dimension dimension = new Dimension(width, height);
+        getDriver().manage().window().setSize(dimension);
+    }
 
-  @Then("I accept alert")
-  public static void iAcceptAlert() {
-    getDriver().switchTo().alert().accept();
-  }
+    @Then("I wait for {int} sec")
+    public static void iWaitForSec(int sec) throws Exception {
+        Thread.sleep(sec * 1000);
+    }
 
-  @Then("I dismiss alert")
-  public void iDismissAlert() {
-    getDriver().switchTo().alert().dismiss();
-  }
+    @When("I maximize window")
+    public static void iMaximizeWindow() {
+        java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension maxWindowSize = new Dimension((int) screenSize.getWidth(), (int) screenSize.getHeight());
+        getDriver().manage().window().setPosition(new Point(0, 0));
+        getDriver().manage().window().setSize(maxWindowSize);
+    }
 
-  @When("I clear element with xpath {string}")
-  public void iClearElementWithXpath(String xpath) {
-    getDriver().findElement(By.xpath(xpath)).clear();
-  }
+    @Then("element with xpath {string} should be present")
+    public static void elementWithXpathShouldBePresent(String xpath) {
+        assertThat(getDriver().findElements(By.xpath(xpath))).hasSize(1);
+    }
 
-  @Then("I should see page title as {string}")
-  public void iShouldSeePageTitleAs(String title) {
-    assertThat(getDriver().getTitle()).isEqualTo(title);
-  }
+    @Then("element with xpath {string} should not be present")
+    public static void elementWithXpathShouldNotBePresent(String xpath) {
+        assertThat(getDriver().findElements(By.xpath(xpath))).hasSize(0);
+    }
 
-  @Then("I should see page title with {string}")
-  public void iShouldSeePageTitleWith(String title) {
-    assertThat(getDriver().getTitle()).contains(title);
-  }
+    @Then("I wait for element with xpath {string} to be present")
+    public static void iWaitForElementWithXpathToBePresent(String xpath) {
+        new WebDriverWait(getDriver(), 10, 200).until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+    }
+    @Then("I wait for element with xpath {string} to be displayed")
+    public static void iWaitForElementWithXpathToBeDisplayed(String xpath) {
+        new WebDriverWait(getDriver(), 10, 200).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+    }
 
-  @Then("I should see page title as not {string}")
-  public void iShouldNotSeePageTitleAsNot(String title) {
-    assertThat(getDriver().getTitle()).isNotEqualTo(title);
-  }
+    @Then("I wait for element with xpath {string} to not be present")
+    public static void iWaitForElementWithXpathToNotBePresent(String xpath) {
+        new WebDriverWait(getDriver(), 10, 200).until(ExpectedConditions.not(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath))));
+    }
 
-  @Then("I should see page title contains {string}")
-  public void iShouldSeePageTitleContains(String title) {
-    assertThat(getDriver().getTitle()).contains(title);
-  }
+    @Then("element with xpath {string} should be displayed")
+    public static void elementWithXpathShouldBeDisplayed(String xpath) {
+        assertThat(getDriver().findElement(By.xpath(xpath)).isDisplayed()).isTrue();
+    }
 
-  @Then("I should see page title does not contain {string}")
-  public void iShouldSeePageTitleDoesNotContain(String title) {
-    assertThat(getDriver().getTitle()).doesNotContain(title);
-  }
+    @Then("element with xpath {string} should not be displayed")
+    public static void elementWithXpathShouldNotBeDisplayed(String xpath) {
+        assertThat(getDriver().findElement(By.xpath(xpath)).isDisplayed()).isFalse();
+    }
 
-  @Then("I scroll to the element with xpath {string} with offset {int}")
-  public void iScrollToTheElementWithXpathWithOffset(String xpath, int offset) throws Exception {
-    WebElement element = getDriver().findElement(By.xpath(xpath));
-    JavascriptExecutor executor = (JavascriptExecutor) getDriver();
-    executor.executeScript("arguments[0].scrollIntoView(false);", element);
-    executor.executeScript("window.scrollBy(0, " + offset + ");", element);
-    Thread.sleep(500);
-  }
+    @Then("element with xpath {string} should be enabled")
+    public static void elementWithXpathShouldBeEnabled(String xpath) {
+        assertThat(getDriver().findElement(By.xpath(xpath)).isEnabled()).isTrue();
+    }
 
-  @When("I mouse over element with xpath {string}")
-  public void iMouseOverElementWithXpath(String xpath) {
-    new Actions(getDriver()).moveToElement(getDriver().findElement(By.xpath(xpath))).perform();
-  }
+    @Then("element with xpath {string} should be disabled")
+    public static void elementWithXpathShouldBeDisabled(String xpath) {
+        assertThat(getDriver().findElement(By.xpath(xpath)).isEnabled()).isFalse();
+    }
+
+    @Then("element with xpath {string} should be selected")
+    public static void elementWithXpathShouldBeSelected(String xpath) {
+        assertThat(getDriver().findElement(By.xpath(xpath)).isSelected()).isTrue();
+    }
+
+    @Then("element with xpath {string} should not be selected")
+    public static void elementWithXpathShouldNotBeSelected(String xpath) {
+        assertThat(getDriver().findElement(By.xpath(xpath)).isSelected()).isFalse();
+    }
+
+    @When("I type {string} into element with xpath {string}")
+    public static void iTypeIntoElementWithXpath(String text, String xpath) {
+        getDriver().findElement(By.xpath(xpath)).sendKeys(text);
+    }
+
+    @Then("I click on element with xpath {string}")
+    public static void iClickOnElementWithXpath(String xpath) {
+        getDriver().findElement(By.xpath(xpath)).click();
+    }
+
+    @Then("I click on element using JavaScript with xpath {string}")
+    public static void iClickOnElementUsingJavaScriptWithXpath(String xpath) {
+        WebElement element = getDriver().findElement(By.xpath(xpath));
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        executor.executeScript("arguments[0].click();", element);
+    }
+
+    @Then("I take screenshot")
+    public static void iTakeScreenshot() throws Exception {
+        TakesScreenshot screenshotTaker = (TakesScreenshot) getDriver();
+        File screenshot = screenshotTaker.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot, new File("target/screenshot " + new Date() + ".png"));
+    }
+
+    @Then("element with xpath {string} should have text as {string}")
+    public static void elementWithXpathShouldHaveTextAs(String xpath, String text) {
+        String actualText = getDriver().findElement(By.xpath(xpath)).getText();
+        assertThat(actualText).isEqualTo(text);
+    }
+
+    @Then("element with xpath {string} should not have text as {string}")
+    public static void elementWithXpathShouldNotHaveTextAs(String xpath, String text) {
+        String actualText = getDriver().findElement(By.xpath(xpath)).getText();
+        assertThat(actualText).isNotEqualTo(text);
+    }
+
+    @Then("element with xpath {string} should contain text {string}")
+    public static void elementWithXpathShouldContainText(String xpath, String text) {
+        String actualText = getDriver().findElement(By.xpath(xpath)).getText();
+        // String textContent = getDriver().findElement(By.xpath(xpath)).getAttribute("textContent");
+        // String innerHTML = getDriver().findElement(By.xpath(xpath)).getAttribute("innerHTML");
+        assertThat(actualText).containsIgnoringCase(text);
+    }
+
+    @Then("element with xpath {string} should not contain text {string}")
+    public static void elementWithXpathShouldNotContainText(String xpath, String text) {
+        String actualText = getDriver().findElement(By.xpath(xpath)).getText();
+        assertThat(actualText).doesNotContain(text);
+    }
+
+    @Then("element with xpath {string} should have attribute {string} as {string}")
+    public static void elementWithXpathShouldHaveAttributeAs(String xpath, String attribute, String attributeValue) {
+        String value = getDriver().findElement(By.xpath(xpath)).getAttribute(attribute);
+        String attrValue = attributeValue;
+        assertThat(getDriver().findElement(By.xpath(xpath)).getAttribute(attribute)).isEqualTo(attributeValue);
+    }
+
+    @Then("element with xpath {string} should not have attribute {string} as {string}")
+    public static void elementWithXpathShouldNotHaveAttributeAs(String xpath, String attribute, String attributeValue) {
+        assertThat(getDriver().findElement(By.xpath(xpath)).getAttribute(attribute)).isNotEqualTo(attributeValue);
+    }
+
+    @Then("I switch to iframe with xpath {string}")
+    public static void iSwitchToIframeWithXpath(String xpath) {
+        getDriver().switchTo().frame(getDriver().findElement(By.xpath(xpath)));
+    }
+
+    @Then("I switch to default content")
+    public static void iSwitchToDefaultContent() {
+        getDriver().switchTo().defaultContent();
+    }
+
+    @Then("I switch to new window")
+    public static void iSwitchToNewWindow() {
+        Iterator<String> iterator = getDriver().getWindowHandles().iterator();
+        String newWindow = iterator.next();
+        while (iterator.hasNext()) {
+            newWindow = iterator.next();
+        }
+        getDriver().switchTo().window(newWindow);
+    }
+
+    @Then("I switch to first window")
+    public static void iSwitchToFirstWindow() {
+        getDriver().switchTo().window(getDriver().getWindowHandles().iterator().next());
+    }
+
+    @Then("I accept alert")
+    public static void iAcceptAlert() {
+        getDriver().switchTo().alert().accept();
+    }
+
+    @Then("I dismiss alert")
+    public static void iDismissAlert() {
+        getDriver().switchTo().alert().dismiss();
+    }
+
+    @When("I clear element with xpath {string}")
+    public static void iClearElementWithXpath(String xpath) {
+        getDriver().findElement(By.xpath(xpath)).clear();
+    }
+
+    @Then("I should see page title as {string}")
+    public static void iShouldSeePageTitleAs(String title) {
+        assertThat(getDriver().getTitle()).isEqualTo(title);
+    }
+
+    @Then("I should see page title with {string}")
+    public static void iShouldSeePageTitleWith(String title) {
+        assertThat(getDriver().getTitle()).contains(title);
+    }
+
+    @Then("I should see page title as not {string}")
+    public static void iShouldNotSeePageTitleAsNot(String title) {
+        assertThat(getDriver().getTitle()).isNotEqualTo(title);
+    }
+
+    @Then("I should see page title contains {string}")
+    public static void iShouldSeePageTitleContains(String title) {
+        assertThat(getDriver().getTitle()).contains(title);
+    }
+
+    @Then("I should see page title does not contain {string}")
+    public static void iShouldSeePageTitleDoesNotContain(String title) {
+        assertThat(getDriver().getTitle()).doesNotContain(title);
+    }
+
+    @Then("I scroll to the element with xpath {string} with offset {int}")
+    public static void iScrollToTheElementWithXpathWithOffset(String xpath, int offset) throws Exception {
+        WebElement element = getDriver().findElement(By.xpath(xpath));
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        executor.executeScript("arguments[0].scrollIntoView(false);", element);
+        executor.executeScript("window.scrollBy(0, " + offset + ");", element);
+        Thread.sleep(500);
+    }
+
+    @When("I mouse over element with xpath {string}")
+    public static void iMouseOverElementWithXpath(String xpath) {
+        new Actions(getDriver()).moveToElement(getDriver().findElement(By.xpath(xpath))).perform();
+    }
 
 }
--- src/test/java/definitions/USPSStepDefs.java
+++ src/test/java/definitions/USPSStepDefs.java
@@ -4,11 +4,19 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static support.TestContext.getDriver;
 
 public class USPSStepDefs {
+    public static final String MOBILE_HAMBURGER_ACTIVE_DROPDOWN_MENU_XPATH = "//a[@class='mobile-hamburger active']";
     private static final String STREET_XPATH = "//input[@id='tAddress']";
     private static final String STATE_XPATH = "//select[@id='tState']";
     private static final String CITY_XPATH = "//input[@id='tCity']";
@@ -18,20 +26,74 @@ public class USPSStepDefs {
     private static final String FIND_BUTTON_XPATH = "//a[@id='zip-by-address']";
     private static final String HEADER_ZIPCODE_BY_ADDRESS_XPATH = "//div[@class='zip_code_address_results active']//h2";
     private static final String HEADER_ZIPCODE_BY_ADDRESS_TEXT = "ZIP Code™ by Address";
+    private static final String HEADER_LOOK_UP_A_ZIP_CODE_TEXT = "Look Up a ZIP Code™";
     private static final String ZIPCODE_BY_RESULTS_ADDRESS_XPATH = "//div[@class='zipcode-result-address']";
+    public static final String NAV_LIST_XPATH = "//ul[@class='nav-list']";
+    public static final String A_CLASS_MOBILE_HAMBURGER_IMG_XPATH = "//a[@class='mobile-hamburger']/img";
+    public static final String NAVQUICKTOOLS_A_ARIA_EXPANDED_FALSE_XPATH = "//a[@name='navquicktools']/../a[@aria-expanded='false']";
+    public static final String NAVQICKTOOLS_MENU_ITEM_XPATH = "//a[@name='navquicktools']/../a[@aria-expanded='false']/..//div[@class='active']//li//img";
+    public static final String NAVIGATION_QUICKTOOLS_MENU_ITEM_XPATH = "//a[@name='navquicktools']//..//a[@class='nav-first-element menuitem']";
+    public static final String ZIPCODE_LOOK_IN_NAVQUICKTOOLS_MENU_XPATH = "//a[@name='navquicktools']//..//a[@class='nav-first-element menuitem']//..//li/a/img";
+    public static final String ZIPCODE_LOOK_UP_ICON_NAVIGATION_QUICKTOOLS_TEXT = "Zip Code™ Lookup Icon";
 
-    @When("I go to Lookup ZIP page by address")
     public void iGoToLookupZIPPageByAddressByResizingInitialWindow() {
+        System.out.println("I run from resizing initial window and ");
         PredefinedStepDefs.iWaitForElementWithXpathToBePresent(ZIP_CODE_LINK_XPATH);
         PredefinedStepDefs.iResizeWindowToAnd(700, 700);
         QuoteStepDefs.click(ZIP_CODE_LINK_XPATH);
+        iClickByAddress();
+    }
+
+    private void iClickByAddress() {
         PredefinedStepDefs.iWaitForElementWithXpathToBePresent(LOOK_UP_A_ZIP_CODE_XPATH);
-        PredefinedStepDefs.iMaximizeWindow();
+        //PredefinedStepDefs.iMaximizeWindow();
         PredefinedStepDefs.iWaitForElementWithXpathToBePresent(BY_ADDRESS_BUTTON_XPATH);
         QuoteStepDefs.click(BY_ADDRESS_BUTTON_XPATH);
         PredefinedStepDefs.iWaitForElementWithXpathToBePresent(STREET_XPATH);
     }
 
+    @When("I go to Lookup ZIP page by address")
+    public void iGoToLookupZIPPageByAddress() throws Exception {
+        //PredefinedStepDefs.iResizeWindowToAnd(700, 700);
+        if ((QuoteStepDefs.getWebElement(NAV_LIST_XPATH).isDisplayed())) {
+            System.out.println("I run from Navigation list menu");
+            Rectangle res = getDriver().findElement(By.xpath(NAVIGATION_QUICKTOOLS_MENU_ITEM_XPATH)).getRect();
+            System.out.println(res.getX());
+            new Actions(getDriver())
+                    .moveToElement(QuoteStepDefs.getWebElement(NAVIGATION_QUICKTOOLS_MENU_ITEM_XPATH))
+                    .build()
+                    .perform();
+            PredefinedStepDefs.iWaitForSec(4);
+
+            List<WebElement> menuItemList = getDriver().findElements(By.xpath(ZIPCODE_LOOK_IN_NAVQUICKTOOLS_MENU_XPATH));
+            List<String> menuItemAttributes = new ArrayList<>();
+            menuItemList.forEach(el -> menuItemAttributes.add(el.getAttribute("alt")));
+            WebElement zipCodeLookUpIcon = menuItemList.get(menuItemAttributes.indexOf(ZIPCODE_LOOK_UP_ICON_NAVIGATION_QUICKTOOLS_TEXT));
+
+            new Actions(getDriver()).moveToElement(zipCodeLookUpIcon)
+                    .build()
+                    .perform();
+            zipCodeLookUpIcon.click();
+            PredefinedStepDefs.iWaitForSec(4);
+            iClickByAddress();
+
+        } else if ((QuoteStepDefs.getWebElement(A_CLASS_MOBILE_HAMBURGER_IMG_XPATH)).isDisplayed()) {
+            System.out.println("I run from hamburger menu");
+            QuoteStepDefs.click(A_CLASS_MOBILE_HAMBURGER_IMG_XPATH);
+            PredefinedStepDefs.iWaitForElementWithXpathToBeDisplayed(MOBILE_HAMBURGER_ACTIVE_DROPDOWN_MENU_XPATH);
+            QuoteStepDefs.click(NAVQUICKTOOLS_A_ARIA_EXPANDED_FALSE_XPATH);
+
+            List<WebElement> menuItemList = getDriver().findElements(By.xpath(NAVQICKTOOLS_MENU_ITEM_XPATH));
+            List<String> menuItemAttributes = new ArrayList<>();
+            menuItemList.forEach(el -> {
+                menuItemAttributes.add(el.getAttribute("alt"));
+                System.out.println(el.getAttribute("alt"));
+            });
+            menuItemList.get(menuItemAttributes.indexOf(ZIPCODE_LOOK_UP_ICON_NAVIGATION_QUICKTOOLS_TEXT)).click();
+            iClickByAddress();
+
+        } else iGoToLookupZIPPageByAddressByResizingInitialWindow();
+    }
 
     @And("I fill out {string} street, {string} city, {string} state")
     public void iFillOutStreetCityState(String street, String city, String state) {
@@ -44,7 +106,7 @@ public class USPSStepDefs {
     @Then("I validate {string} zip code exists in the result")
     public void iValidateZipCodeExistsInTheResult(String zipcode) {
         PredefinedStepDefs.iWaitForElementWithXpathToBePresent(HEADER_ZIPCODE_BY_ADDRESS_XPATH);
-        assertTrue(QuoteStepDefs.getText(HEADER_ZIPCODE_BY_ADDRESS_XPATH).equals(HEADER_ZIPCODE_BY_ADDRESS_TEXT));
+        assertEquals(HEADER_ZIPCODE_BY_ADDRESS_TEXT, QuoteStepDefs.getText(HEADER_ZIPCODE_BY_ADDRESS_XPATH));
         getDriver().findElements(By.xpath(ZIPCODE_BY_RESULTS_ADDRESS_XPATH))
                 .forEach(el -> {
                     assertTrue(el.getText().contains(zipcode));
