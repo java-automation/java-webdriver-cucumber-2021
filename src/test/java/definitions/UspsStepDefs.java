a24600b7f856d3815205dfd23baadc2b4929637f
Dmitry Igumnov <53914429+ehnat0n@users.noreply.github.com>
2021-12-13 13:52:15 -0700

    Added two USPS scenarios: calc price & verify sign-in req

--- src/test/java/definitions/UspsStepDefs.java
+++ src/test/java/definitions/UspsStepDefs.java
@@ -22,7 +22,7 @@ public class UspsStepDefs {
         WebElement sendButton = getDriver().findElement(By.xpath("//a[@id='mail-ship-width']"));
         switch (strategy) {
             case "quick tools" -> {
-                getDriver().findElement(By.xpath("//ul[@class='nav-list']//a[contains(@class,'nav-first-element')]")).click();
+                getDriver().findElement(By.xpath("//ul[@class='nav-list']/li[contains(@class,'qt-nav')]")).click();
                 getDriver().findElement(By.xpath("//a[@role='menuitem']/img[contains(@alt,'Zip Code')]")).click();
             }
             case "send" -> {
@@ -53,5 +53,72 @@ public class UspsStepDefs {
         new WebDriverWait(getDriver(), 3).until(ExpectedConditions.visibilityOf(getDriver().findElement(By.xpath(containerXPath))));
         List<WebElement> results = getDriver().findElements(By.xpath(containerXPath + "//li[contains(@class,'list-group-item')]"));
         for (WebElement el : results) assertThat(el.getText()).contains(zip);
+    }
+
+    @When("I go to Calculate Price Page")
+    public void iGoToCalculatePricePage() {
+        new Actions(getDriver()).moveToElement(getDriver().findElement(By.xpath("//ul[@class='nav-list']/li[contains(@class,'qt-nav')]"))).perform();
+        getDriver().findElement(By.xpath("//a[@role='menuitem']/img[contains(@alt,'Calculate a Price')]")).click();
+    }
+
+    @And("I select {string} with {string} shape")
+    public void iSelectWithShape(String country, String shape) {
+        new Select(getDriver().findElement(By.xpath("//select[@id='CountryID']"))).selectByVisibleText(country);
+        getDriver().findElement(By.xpath("//input[@value='" + shape + "']")).click();
+    }
+
+    @And("I define {string} quantity")
+    public void iDefineQuantity(String quantity) {
+        getDriver().findElement(By.xpath("//input[@id='quantity-0']")).sendKeys(quantity);
+    }
+
+    @Then("I calculate the price and validate cost is {string}")
+    public void iCalculateThePriceAndValidateCostIs(String totalCost) {
+        getDriver().findElement(By.xpath("//input[@value='Calculate']")).click();
+        assertThat(getDriver().findElement(By.xpath("//*[@id='total']")).getText()).isEqualTo(totalCost);
+    }
+
+    @When("I perform {string} search")
+    public void iPerformSearch(String query) {
+        String navSearchXPath = "//li[contains(@class,'nav-search')]";
+        new Actions(getDriver()).moveToElement(getDriver().findElement(By.xpath(navSearchXPath))).perform();
+        getDriver().findElement(By.xpath(navSearchXPath + "//input[@name='q']")).sendKeys(query);
+        getDriver().findElement(By.xpath(navSearchXPath + "//input[@value='Search']")).click();
+    }
+
+    @And("I set {string} in filters")
+    public void iSetInFilters(String filterName) {
+        By sendFilter = By.xpath("//p[@title='" + filterName + "']");
+        new WebDriverWait(getDriver(), 3).until(ExpectedConditions.visibilityOfElementLocated(sendFilter));
+        getDriver().findElement(sendFilter).click();
+    }
+
+    @Then("I verify that {string} results found")
+    public void iVerifyThatResultsFound(String amountOfResults) {
+        WebElement spinner = getDriver().findElement(By.xpath("//div[@class='spinner-content']"));
+        new WebDriverWait(getDriver(),3).until(ExpectedConditions.invisibilityOf(spinner));
+        List<WebElement> results = getDriver().findElements(By.xpath("//ul[@id='records']/li"));
+        assertThat(results.size()).isEqualTo(Integer.parseInt(amountOfResults));
+    }
+
+    @When("I select {string} in results")
+    public void iSelectInResults(String resultToChoose) {
+        getDriver().findElement(By.xpath("//ul[@id='records']//a//*[contains(normalize-space(),'" + resultToChoose + "')]")).click();
+    }
+
+    @And("I click {string} button")
+    public void iClickButton(String buttonText) {
+        getDriver().findElement(By.xpath("//a[@class='button--primary'][contains(normalize-space(),'" + buttonText + "')]")).click();
+    }
+
+    @Then("I validate that Sign In is required")
+    public void iValidateThatSignInIsRequired() {
+        for (String handle : getDriver().getWindowHandles()) {
+            getDriver().switchTo().window(handle);
+            if (getDriver().getCurrentUrl().contains("https://www.usps.com/ship/priority-mail")) continue;
+            assertThat(getDriver().getCurrentUrl()).contains("https://reg.usps.com");
+            assertThat(getDriver().findElement(By.xpath("//button[@id='btn-submit']"))).isNotNull();
+            assertThat(getDriver().findElement(By.xpath("//a[@id='sign-up-button']"))).isNotNull();
+        }
     }
 }
--- src/test/resources/features/usps.feature
+++ src/test/resources/features/usps.feature
@@ -8,3 +8,21 @@ Feature: USPS website scenarios
     When  I go to Lookup ZIP page by address through "mouseover"
     *     I fill out "4970 El Camino Real" street, "Los Altos" city, "CA" state
     Then  I validate "94022" zip code exists in all results
+
+  @usps2
+  Scenario: Calculate price
+    Given I go to "usps" page
+    When  I go to Calculate Price Page
+    *     I select "Canada" with "Postcard" shape
+    *     I define "2" quantity
+    Then  I calculate the price and validate cost is "$2.60"
+
+  @usps3
+  Scenario: Verify location
+    Given I go to "usps" page
+    When  I perform "Free Boxes" search
+    *     I set "Send" in filters
+    Then  I verify that "7" results found
+    When  I select "Priority Mail | USPS" in results
+    *     I click "Ship Now" button
+    Then  I validate that Sign In is required
