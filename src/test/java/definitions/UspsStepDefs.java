98131cc489a2f1211efc3a342eb3f801bebf120a
alex_ya <alegz.nn@gmail.com>
2021-12-16 02:25:26 -0800

    USPS site - Free Boxes search

--- src/test/java/definitions/UspsStepDefs.java
+++ src/test/java/definitions/UspsStepDefs.java
@@ -6,7 +6,10 @@ import io.cucumber.java.en.When;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -39,4 +42,76 @@ public class UspsStepDefs {
     }
 
     }
+
+    @When("I go to Calculate Price Page")
+    public void iGoToCalculatePricePage() {
+        WebElement sendMenu = getDriver().findElement(By.xpath("//a[@id='mail-ship-width']"));
+        new Actions(getDriver()).moveToElement(sendMenu).perform();
+        getDriver().findElement(By.xpath("//li[@class='tool-calc']/a[contains(@href,'postcalc.usps.com')]")).click();
+
+    }
+
+    @And("I select {string} with {string} shape")
+    public void iSelectWithShape(String country, String mailType) {
+        WebElement selectElement = getDriver().findElement(By.xpath("//select[@id='CountryID']"));
+        new Select(selectElement).selectByVisibleText(country);
+        getDriver().findElement(By.xpath("//input[@id='option_1']")).click();
+
+    }
+
+
+    @And("I define {string} quantity")
+    public void iDefineQuantity(String quantity) {
+        getDriver().findElement(By.xpath("//input[@id='quantity-0']")).sendKeys(quantity);
+        getDriver().findElement(By.xpath("//input[@value='Calculate']")).click();
+    }
+
+    @Then("I calculate the price and validate cost is {string}")
+    public void iCalculateThePriceAndValidateCostIs(String cost) {
+        String costResult = getDriver().findElement(By.xpath("//div[@id='total']")).getText();
+        assertThat(costResult).isEqualTo(cost);
+    }
+
+    @When("I perform {string} search")
+    public void iPerformSearch(String search) {
+//        getDriver().findElement( By.xpath("//li[@class='nav-search menuheader']")).click();
+        WebElement searchMenu = getDriver().findElement(By.xpath("//li[@class='nav-search menuheader']"));
+        new Actions(getDriver()).moveToElement(searchMenu).perform();
+        getDriver().findElement(By.xpath("//a[contains(@href, 'Free%20Boxes')]")).click();
+    }
+
+    @And("I set {string} in filters")
+    public void iSetInFilters(String send) {
+        getDriver().findElement(By.xpath("//p[@title='Send']")).click();
+    }
+
+    @Then("I verify that {string} results found")
+    public void iVerifyThatResultsFound(String results) {
+        List<WebElement> searchResults = getDriver().findElements(By.xpath("//ul[@id='records']/li"));
+        assertThat(searchResults.size()).isEqualTo(7);
+//        if (searchResults.size() == 7){
+//            System.out.println("7 results found");
+//        } else {
+//            System.out.println("results number doesn't match");
+//        }
+    }
+
+    @When("I select {string} in results")
+    public void iSelectInResults(String arg0) {
+        getDriver().findElement(By.xpath("//span[@id='title_22']")).click();
+    }
+
+    @And("I click {string} button")
+    public void iClickButton(String arg0) {
+        getDriver().findElement(By.xpath("//a[contains(@href,'cns')][contains(@class,'button--primary')]")).click();
+    }
+
+    @Then("I validate that Sign In is required")
+    public void iValidateThatSignInIsRequired() {
+        ArrayList<String> tabs2 = new ArrayList<String>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(tabs2.get(1));
+            String title = getDriver().getTitle();
+            assertThat(title).contains("Sign In");
+
+        }
 }
