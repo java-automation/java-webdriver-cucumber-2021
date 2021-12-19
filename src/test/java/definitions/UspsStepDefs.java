4483fd1e111201985842dab587c70b1d8bf5cfc8
Hrag Bardizbanian <hragbardizbanian@gmail.com>
2021-12-18 17:54:52 -0800

    usps 1- 3

--- src/test/java/definitions/UspsStepDefs.java
+++ src/test/java/definitions/UspsStepDefs.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static java.lang.Thread.sleep;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -56,15 +58,22 @@ public class UspsStepDefs {
 
     @And("I select {string} with {string} shape")
     public void iSelectWithShape(String country, String shape) throws InterruptedException {
-
-
        getDriver().findElement(By.xpath("//select[@id='CountryID']/option[text()='" + country + "']")).click();
-
        getDriver().findElement(By.xpath("//div[@class='col-xs-6 col-sm-3 col-md-2']//input[@id='option_1']")).click();
 
 
+    }
 
+    @And("I define {string} quantity")
+    public void iDefineQuantity(String quantity) {
+        getDriver().findElement(By.xpath("//input[@id='quantity-0']")).sendKeys(quantity);
+        getDriver().findElement(By.xpath("//input[@type='button']")).click();
+    }
 
-
+    @Then("I calculate the price and validate cost is {string}")
+    public void iCalculateThePriceAndValidateCostIs(String price) {
+        WebElement total = getDriver().findElement(By.xpath("//div[@id='total']"));
+        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
+        wait.until(ExpectedConditions.visibilityOf(total));
     }
 }
--- src/test/resources/features/usps.feature
+++ src/test/resources/features/usps.feature
@@ -16,3 +16,13 @@
       And I select "Canada" with "Postcard" shape
       And I define "2" quantity
       Then I calculate the price and validate cost is "$2.60"
+
+    @usps3
+    Scenario: Verify location
+      Given I go to "usps" page
+      When I perform "Free Boxes" search
+      And I set "Send" in filters
+      Then I verify that "7" results found
+      When I select "Priority Mail | USPS" in results
+      And I click "Ship Now" button
+      Then I validate that Sign In is required
