14494cd153cd78d404cbf2b3fc14bd171a3cb439
Hrag Bardizbanian <hragbardizbanian@gmail.com>
2021-12-18 19:24:45 -0800

    Homeword Done usps!

--- src/test/java/definitions/UspsStepDefs.java
+++ src/test/java/definitions/UspsStepDefs.java
@@ -10,22 +10,24 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.nio.charset.StandardCharsets;
+
 import static java.lang.Thread.sleep;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static support.TestContext.getDriver;
 
 public class UspsStepDefs {
     @When("I go to Lookup ZIP page by address")
     public void iGoToLookupZIPPageByAddress() throws InterruptedException {
-       WebElement sendMenu = getDriver().findElement(By.xpath("//a[@id='mail-ship-width']"));
-       Actions actions = new Actions(getDriver());
-       actions.moveToElement(sendMenu).perform();
+        WebElement sendMenu = getDriver().findElement(By.xpath("//a[@id='mail-ship-width']"));
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(sendMenu).perform();
 
         getDriver().findElement(By.xpath("//a[text()= 'Look Up a ZIP Code']")).click();
         getDriver().findElement(By.xpath("//a[contains(text(),'Find by Address')]")).click();
 
-       //  getDriver().findElement(By.xpath("//h2[@class='header-2 center']//a[text()='Look Up a ZIP Code']")).click();
-       // getDriver().findElement(By.xpath("//a[contains(text(),'Find by Address')]")).click();
+        //  getDriver().findElement(By.xpath("//h2[@class='header-2 center']//a[text()='Look Up a ZIP Code']")).click();
+        // getDriver().findElement(By.xpath("//a[contains(text(),'Find by Address')]")).click();
     }
 
     @And("I fill out {string} street, {string} city, {string} state")
@@ -58,8 +60,8 @@ public class UspsStepDefs {
 
     @And("I select {string} with {string} shape")
     public void iSelectWithShape(String country, String shape) throws InterruptedException {
-       getDriver().findElement(By.xpath("//select[@id='CountryID']/option[text()='" + country + "']")).click();
-       getDriver().findElement(By.xpath("//div[@class='col-xs-6 col-sm-3 col-md-2']//input[@id='option_1']")).click();
+        getDriver().findElement(By.xpath("//select[@id='CountryID']/option[text()='" + country + "']")).click();
+        getDriver().findElement(By.xpath("//div[@class='col-xs-6 col-sm-3 col-md-2']//input[@id='option_1']")).click();
 
 
     }
@@ -78,5 +80,63 @@ public class UspsStepDefs {
 
         String resultString = total.getText();
         assertThat(resultString).contains(price);
+    }
+
+    @When("I perform {string} search")
+    public void iPerformSearch(String search) {
+        WebElement searchBar = getDriver().findElement(By.xpath("//a[contains(text(),'Search USPS.com')]"));
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(searchBar).perform();
+        getDriver().findElement(By.xpath("//input[@id='global-header--search-track-search']")).sendKeys(search + "\n"); // Enter button
+
+
+    }
+
+    @And("I set {string} in filters")
+    public void iSetInFilters(String send) {
+//        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
+//        wait.until(ExpectedConditions.visibilityOf(getDriver().findElement(By.xpath("//div[@id='dyn_nav_col']"))));
+//        WebElement filter =getDriver().findElement(By.xpath("//div//label[text()='"+filterOption+"']"));
+//        wait.until(ExpectedConditions.visibilityOf(filter)).click();
+//        wait.until(ExpectedConditions.visibilityOf(getDriver().findElement(By.xpath("//span[@id='searchResultsHeading']"))));
+
+        By filter = By.xpath("//p[@title='" + send + "']");
+        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(filter)).click();
+        wait.until(ExpectedConditions.visibilityOf(getDriver().findElement(By.xpath("//span[@id='searchResultsHeading']"))));
+    }
+
+    @Then("I verify that {string} results found")
+    public void iVerifyThatResultsFound(String numOfResults) {
+        WebElement searchResults = getDriver().findElement(By.xpath("//span[@id='searchResultsHeading']"));// to locate the result number
+        String number = searchResults.getText();
+        assertThat(number).contains(numOfResults + " results found for");
+    }
+
+    @When("I select {string} in results")
+    public void iSelectInResults(String link) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
+        wait.until(ExpectedConditions.visibilityOf(getDriver().findElement(By.xpath("//div[@class='search-results']"))));
+        getDriver().findElement(By.xpath("//span[contains(text(),'" + link + "')]")).click();
+    }
+
+    @And("I click {string} button")
+    public void iClickButton(String button) {
+        getDriver().findElement(By.xpath("//a[contains(text(),'" + button + "')]")).click();
+
+    }
+
+    @Then("I validate that Sign In is required")
+    public void iValidateThatSignInIsRequired() {
+
+        for (String handle : getDriver().getWindowHandles()) {
+            getDriver().switchTo().window(handle);
+
+            if (getDriver().getTitle().equals("USPS.com® - Sign In")) {
+                String signIn = getDriver().findElement(By.xpath("//h1[@id='sign-in-to-your-account-header']")).getText();
+                assertThat(signIn).contains("Sign In To Your Account");
+                System.out.println(signIn);
+            }
+        }
     }
 }
