af9755bd6c93722c67cbd2659d3f172a9ed841a7
VictorZ <vctvzv@gmail.com>
2021-12-20 17:47:40 -0800

    Day16_HW_v2
    //Scenario: Calculate price
    //Given I go to "usps" page
    //When I go to Calculate Price Page
    //And I select "Canada" with "Postcard" shape
    //And I define "2" quantity
    //Then I calculate the price and validate cost is "$2.60"

--- src/test/java/definitions/Day16hwStepDefs.java
+++ src/test/java/definitions/Day16hwStepDefs.java
@@ -1,8 +1,17 @@
 package definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static support.TestContext.getDriver;
 
 public class Day16hwStepDefs {
 
@@ -64,13 +73,45 @@ public class Day16hwStepDefs {
     }
 
 
-
     @Given("I write three lines code to find two max numbers in the array")
     public void iWriteThreeLinesCodeToFindTwoMaxNumbersInTheArray() {
 
         int[] ar1 = {3, 7, -3, -2, 9, 5, 8, 9};
         Arrays.sort(ar1);
         System.out.println(String.format("First Max: %d, Second Max: %d", ar1[ar1.length - 1], ar1[ar1.length - 2]));
+    }
+
+    @When("I go to Calculate Price Page")
+    public void iGoToCalculatePricePage() {
+        getDriver().findElement(By.xpath("//a[@id='mail-ship-width']")).click();
+        getDriver().findElement(By.xpath("//a[contains(@href,'calculateretailpostage')]")).click();
+    }
+
+
+    @And("I select Canada with Postcard shape")
+    public void iSelectCanadaWithPostcardShape() {
+        getDriver().findElement(By.xpath("//select[@id='CountryID']/option [@value='10054']")).click();
+        getDriver().findElement(By.xpath("//input [@value='Postcard']")).click();
+    }
+
+    @And("I define {string} quantity")
+    public void iDefineQuantity(String arg0) {
+        getDriver().findElement(By.xpath("//input[@id='quantity-0']")).sendKeys(arg0);
+    }
+
+    @Then("I calculate the price and validate cost is {string}")
+    public void iCalculateThePriceAndValidateCostIs(String price) {
+        getDriver().findElement(By.xpath("//input[@value='Calculate']")).click();
+        WebElement resultContainer = getDriver().findElement(By.xpath("//div[@id='total']"));
+
+        WebDriverWait waitSec = new WebDriverWait(getDriver(), 5);
+        waitSec.until(webDriver -> resultContainer.getText().length() > 0);
+
+        String resultString = resultContainer.getText();
+        System.out.println(resultString);
+
+        assertThat(resultString).contains(price);
+
     }
 }
 
--- src/test/resources/features/d16_hw_java.feature
+++ src/test/resources/features/d16_hw_java.feature
@@ -1,18 +1,33 @@
-@d15_hw_Java
+@d16_hw_Java
 Feature: Java feature
 
-  @d15_hw_Java1
+  @d16_hw_Java1
   Scenario: Function that finds 2 max numbers in the array
     Given I find two max numbers in the array
 
-  @d15_hw_Java2
+  @d16_hw_Java2
   Scenario: Function that determines palindrome
     Given I check if "Kayak" is the palindrome
 
-  @d15_hw_Java3
+  @d16_hw_Java3
   Scenario: Function that finds if array contains duplicates
     Given I check if an Arraycontains duplicates
 
-  @d15_hw_Java4
+  @d16_hw_Java4
   Scenario: Function that finds 2 max numbers in the array; v2
     Given I write three lines code to find two max numbers in the array
+
+  @d16_hw_Java5
+  Scenario: Calculate price
+    Given I open "usps" page
+    And I wait & check for 1 sec
+    When I go to Calculate Price Page
+    And I select Canada with Postcard shape
+    And I define "2" quantity
+    #And I wait & check for 1 sec
+    Then I calculate the price and validate cost is "$2.60"
+    And I wait & check for 2 sec
+
+
+
+
