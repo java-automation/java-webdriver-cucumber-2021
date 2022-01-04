0d54c99a32002bc36bfc60d2117ca0c9359c62ff
Gavrilova <gavrilova.irina@gmail.com>
2022-01-04 00:08:11 -0600

    Creating new class Cat and working with it: - pages/Cat.java; - cat.feature; - CatStepDefs.java.

---
+++ src/test/java/pages/Cat.java
@@ -0,0 +1,43 @@
+package pages;
+
+public class Cat {
+    //fields
+    private String name;
+
+    public void setName(String newName) {
+        if ((!newName.isEmpty()) && (!newName.equals("Jerry"))) {
+            name = newName;
+        } else {
+            throw new Error("Unacceptable name! " + newName);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //constructor
+    public Cat() {
+        setName("nameless cat");
+    }
+    public Cat(String newName) {
+        setName(newName);
+    }
+
+    //methods
+    public void walk() {
+        System.out.println("Cat " + name + " is walking!");
+    }
+
+    public void sleep() {
+        System.out.println("Cat " + name + " is sleeping!");
+    }
+
+    public void eat(String what) {
+        System.out.println("Cat " + name + " is eating " + what + "!");
+    }
+
+    public void speak() {
+        System.out.println("Cat " + name + " is meowing!");
+    }
+}
---
+++ src/test/resources/features/cat.feature
@@ -0,0 +1,6 @@
+@cat
+Feature:  Cat class usage
+  @cat1
+  Scenario: cat creating
+    Given I work with classes
+
