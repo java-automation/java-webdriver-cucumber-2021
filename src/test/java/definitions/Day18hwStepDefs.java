76854dae539cc02a16e55f7f3aad37938b87617b
VictorZ <vctvzv@gmail.com>
2022-01-13 23:18:45 -0800

    Day18_HW_v2
    //Code challenge
    //Recursive functions part2
    
    //#1 Recursive function that returns all numbers from n to 1 in reversed order
    //#2 Recursive function that returns the sum of all numbers from n to 1
    //#3 Recursive function that checks string if its a polindrome

--- src/test/java/definitions/Day18hwStepDefs.java
+++ src/test/java/definitions/Day18hwStepDefs.java
@@ -60,4 +60,82 @@ public class Day18hwStepDefs {
         result = n * factorial(n - 1); // 3 * factorial(2)  —▶︎  3 * 2 * factorial(1) —▶ 3 * 2 * 1
         return result;
     }
+
+    @Given("I use recursive function to get all numbers from {int} to 1 in reversed order")
+    //print all numbers from given int n down to 1 in reversed order
+
+    public void getCompositeNumbers(int n) {
+        solveIt(n);
+    }
+
+    public static void solveIt(int n) {
+        if (n == 0) {             // exit/terminate infinitive loop
+            return;
+        }
+        System.out.println(n); // 5 for  ➤  int n = 5;
+        solveIt(n - 1);
+
+//Common approach
+  /*public static void solveIt(int n) {
+        for (int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+    }*/
+    }
+
+    @Given("I use recursive function to get the sum of all numbers from {int} to 1")
+    // given int n ➤ 1 + 2+ ... + (n-1) + n
+    // if n = 3 ➤ 1+2+3=6
+
+    public void getTheSum(int n) {
+        System.out.println(solveIt2(n));
+        // solveIt2(n);
+    }
+
+    private int solveIt2(int n) {
+        if (n == 0) {     // exit/terminate infinitive loop
+            return 0;
+        }
+        return n + solveIt2(n - 1);
+
+//Common approach
+ /* public static void solveIt2(int n) {
+        int res2 = 0;
+        for (int i = 0; i <= n; i++) {
+            res2 += i;
+        }
+        System.out.println(res2);
+    }*/
+    }
+
+
+    @Given("I use recursive function to checks {string} if its a polindrome")
+    //check string if its a polindrome
+
+    public void checkIfPolindrome(String str) {
+        boolean res3 = solveIt3(str);
+        System.out.println(res3);
+    }
+
+    public static boolean solveIt3(String str) {
+        if (str.length() <= 1) {
+            return true;          // everything is OK
+        }
+
+        if (str.charAt(0) == str.charAt(str.length() - 1)) {
+            return solveIt3(str.substring(1, str.length() - 1)); // substring ➤ "aya" for KayaK
+        }
+        return false;
+
+//Common approach
+  /*public static boolean solveIt(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }*/
+    }
 }
+
--- src/test/resources/features/d18_hw_java.feature
+++ src/test/resources/features/d18_hw_java.feature
@@ -12,3 +12,15 @@ Feature: Java feature
   @d18_hw_Java3
   Scenario: Recursive function that returns n!
     Given I use recursive function to get n! of 7
+
+  @d18_hw_Java4
+  Scenario: Recursive function that returns all numbers from n to 1 in reversed order
+    Given I use recursive function to get all numbers from 5 to 1 in reversed order
+
+  @d18_hw_Java5
+  Scenario: Recursive function that returns the sum of all numbers from n to 1
+    Given I use recursive function to get the sum of all numbers from 7 to 1
+
+  @d18_hw_Java6
+  Scenario: Recursive function that checks string if its a polindrome
+    Given I use recursive function to checks "KayaK" if its a polindrome
