320fb75d0a295a803dd9012f2ab6f71d27da7f02
Hrag Bardizbanian <hragbardizbanian@gmail.com>
2022-01-18 09:12:05 -0800

    class material added

---
+++ src/test/java/definitions/TextBox.java
@@ -0,0 +1,16 @@
+package definitions;
+
+public class TextBox {
+
+    //field
+    public String text;
+
+    //methods
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void clear() {
+        text = "";
+    }
+}
